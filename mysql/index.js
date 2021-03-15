4ca45c6da35b12a2ced1a51f0cea31d37e3f8738
= <=>
2021-03-15 14:22:52 +0900

    dsa

--- mysql/index.js
+++ mysql/index.js
@@ -1,10 +1,10 @@
 exports.mysql = ()=>{
     var mysql = require('mysql');
     var db = mysql.createConnection({
-        host:'localhost',
-        user:'root',
-        password:'speed',
-        database:'speed_34'
+        host:'daesunga.cafe24app.com',
+        user:'daesung1080',
+        password:'eotjdzjavjsl!',
+        database:'daesung1080'
     });
 
     return db;
